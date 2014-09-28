606d24ab9e1c06c5caeff1cce4373df976e4d40a
TANG <tangbichang@sunfei.com>
2014-09-28 09:35:42 +0800

    一些小小改动

--- Sprint/library/config.php
+++ Sprint/library/config.php
@@ -1,6 +1,6 @@
 <?php
-    
-    /* 服务器 */ 
+
+    /* 服务器 */
     define('DB_HOST', 'localhost');
 
     /* 数据库用户名 */
@@ -10,12 +10,12 @@
     define('DB_PASSWORD', '');
 
     /* 默认数据库 */
-    define('DB_NAME', 'wordpressdb');
+    define('DB_NAME', 'tangbc');
 
     /* 数据库字符集 */
     define('DB_CHARSET', 'utf8');
 
     /* 时区设置 */
     define('TIMEZONE', "PRC");
-    
+
 ?>
