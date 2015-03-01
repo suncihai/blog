5e998dbb20ca00c43e16f4a3e4890b0c3faf458d
Little Tang <40519806@qq.com>
2015-03-01 21:57:51 +0800

    增加CSS3 Loading模块

---
+++ project/modules/loading.js
@@ -0,0 +1,17 @@
+/**
+ * [CSS3 loading模块]
+ */
+define(function( require, exports ){
+	var $ = require('jquery');
+
+	var Loading = {
+		// config配置参数{type: xxx, width: 100%, height: auto, align: center}
+		init: function( config ) {
+			this.$config = config;
+			switch( config.type ) {
+				case ''
+			}
+		}
+	}
+	exports.base = Loading;
+});
