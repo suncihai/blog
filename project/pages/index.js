9be1b5f83a9b69c818cd411c82bdbe953f1e49a0
Little Tang <40519806@qq.com>
2015-03-29 19:55:07 +0800

    主页文案修改

--- project/pages/index.js
+++ project/pages/index.js
@@ -19,7 +19,7 @@ define(function( require, exports ){
 						'<h2 class="word">欢迎光临鄙人陋舍</h2>',
 						'<h2 class="word">呃······</h2>',
 						'<h2 class="word smaller">主页的设计向来是个头疼的问题</h2>',
-						'<h2 class="word smaller">索性就这样简简单单的</h2>',
+						'<h2 class="word smaller">暂时这样简简单单的吧</h2>',
 						'<h2 class="word smaller">更多内容请移步↓</h2>',
 						'<h2 class="tc">',
 							'<a href="/blog/#matters" class="myBlog">我的博客</a>',
