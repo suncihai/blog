de6cb9cade2760b6faf1ddcd65b5387591838459
TANG <tangbichang@sunfei.com>
2015-04-09 15:28:12 +0800

    modify: 整个框架的字体都用rem作为单位；

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	<title>应用加载中……</title>
 	<script type="text/javascript" src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/blog/dist/sea/sea.js"></script>
 	<script type="text/javascript" src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/blog/dist/core/init.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
 	<style>
 	noscript{
 		font-family: Arial, 'Microsoft Yahei'; display: block; width: 550px; height: 80px;
