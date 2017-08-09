f1230e04fab6c411f5486069a473e06ab8f3ecc0
TANG <40519806@qq.com>
2017-08-09 20:16:14 +0800

    替换百度统计

--- components/CommonFoot.js
+++ components/CommonFoot.js
@@ -1,32 +1,44 @@
+import React from 'react'
+
 const COPY = {
     TEXT: '别往下看了，我就是底线~'
 }
 
-export default () => (
-    <div className="global-foot center">
-        <div className="foot-line"></div>
-        <div className="foot-text center">{ COPY.TEXT }</div>
-        <div hidden>
-            <script src="https://s19.cnzz.com/z_stat.php?id=5606127&web_id=5606127" type="text/javascript"></script>
-        </div>
+export default class extends React.Component {
 
-        <style jsx>{`
-            .foot-line {
-                width: 100%;
-                height: 0;
-                position: absolute;
-                top: 50%;
-                transform: translateY(-50%);
-                border-top: 1px solid #eee;
-            }
-            .foot-text {
-                width: 150px;
-                user-select: none;
-                background: #fff;
-                text-align: center;
-                font-size: 1.2rem;
-                color: #9b9b9b;
-            }
-        `}</style>
-    </div>
-)
+    componentDidMount () {
+        var _hmt = _hmt || []
+        var hm = document.createElement('script')
+        hm.src = 'https://hm.baidu.com/hm.js?76cc38ce96fc554bf5b6ca4047bfd629'
+        var s = document.getElementsByTagName('script')[0]
+        s.parentNode.insertBefore(hm, s)
+    }
+
+    render () {
+        return (
+            <div className="global-foot center">
+                <div className="foot-line"></div>
+                <div className="foot-text center">{ COPY.TEXT }</div>
+
+                <style jsx>{`
+                    .foot-line {
+                        width: 100%;
+                        height: 0;
+                        position: absolute;
+                        top: 50%;
+                        transform: translateY(-50%);
+                        border-top: 1px solid #eee;
+                    }
+                    .foot-text {
+                        width: 150px;
+                        user-select: none;
+                        background: #fff;
+                        text-align: center;
+                        font-size: 1.2rem;
+                        color: #9b9b9b;
+                    }
+                `}</style>
+            </div>
+        )
+    }
+}
