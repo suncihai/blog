e7f1596a9a7fdd49213616157586ba3de19a86e1
TANG <tangbc921@gmail.com>
2017-12-03 10:59:35 +0800

    采用 CSS 绘制的 Loading

--- components/modules/Loading.js
+++ components/modules/Loading.js
@@ -1,9 +1,5 @@
 import styled, { keyframes } from 'styled-components'
 
-const Loading = styled.div`
-    text-align: center;
-    padding: 1em;
-`
 const LoadingAnimation = keyframes`
     from {
         transform: rotate(0deg);
@@ -12,13 +8,26 @@ const LoadingAnimation = keyframes`
         transform: rotate(360deg);
     }
 `
-const LoadingIcon = styled.img`
+const Loading = styled.div`
+    width: 20px;
+    height: 20px;
+    margin: 0 auto;
+    border-radius: 50%;
+    border: 2px solid #333;
     animation: ${LoadingAnimation} 1s infinite linear;
     animation-fill-mode: both;
+`
+const LoadingIcon = styled.div`
+    width: 30px;
+    height: 20px;
+    background: #fff;
+    position: absolute;
+    left: -5px;
+    top: 6px;
 `
 
 export default () => (
     <Loading>
-        <LoadingIcon src="/static/images/loading.svg" />
+        <LoadingIcon />
     </Loading>
 )
